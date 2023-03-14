e1f603380e7e52a1f478b2bd91acd3810206ef58
DomingoMango <l21121601@morelia.tecnm.mx>
2023-03-14 13:20:13 -0600

    si

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -7,37 +7,76 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    public class Banorque
+    public class LoanCalculation
     {
-        public int loanAmount;
-        public int interest;
-        public bool ApplyForLoan(float salary,out int loanAmount,out int interest, params string[]doc ){
-            loanAmount = this.loanAmount;
-            interest = this.interest;
-            foreach(var item in doc)
+        public LoanCalculation()
+        {
+        }
+
+        public bool ApplyForLoan(float salary, out int loanAmount, out int interest, params string[] doc)
+        {
+
+            foreach (var item in doc)
             {
-       
-                if (item.ToLower().Equals("pasaporte")&& doc.Length>=2)
+
+                if (item.ToLower().Equals("pasaporte") && doc.Length >= 2)
                 {
-                    if(salary > 0 && salary<12000) {
+                    if (salary > 0 && salary < 12000)
+                    {
                         Console.WriteLine("Loan Amount: 25000, Interest=4%");
-                    }else if(salary <= 50000 && salary>=12000)
+                        loanAmount = 25000;
+                        interest = 4;
+                        return true;
+                    }
+                    else if (salary <= 50000 && salary >= 12000)
                     {
                         Console.WriteLine("Loan Amount: 50000, Interest=6%");
+                        loanAmount = 50000;
+                        interest = 6;
+                        return true;
                     }
                     else if (salary >= 50000)
                     {
+                        loanAmount = 100000;
+                        interest = 8;
                         Console.WriteLine("Loan Amount: 100000, Interest=8%");
+                        return true;
                     }
-                    
-                    return true;
-                }
-            }
-            return false;
-        }
-        public void Emi_Discount()
-        {
 
+
+                }
+
+            }
+            loanAmount = 0;
+            interest = 0;
+            return false;
+
+        }
+        public void Emi_Discount(int loanAmount, int RelativeMarking, int tenure, int interest, out int emi)
+        {
+            emi = (loanAmount + (loanAmount * interest / 100)) / tenure;
+            if (RelativeMarking == 1)
+            {
+                emi = emi * (emi * 10 / 100);
+            }
+            else if (RelativeMarking == 2)
+            {
+                emi = emi * (emi * 8 / 100);
+            }
+            else if (RelativeMarking == 3)
+            {
+                emi = emi * (emi * 6 / 100);
+            }
+            else if (RelativeMarking > 3)
+            {
+                emi = emi * (emi * 4 / 100);
+            }
+        }
+        public void SalaryDeduction(ref float salary, int relativeMarking, int tenure, int interest, int LoanAmount, out int emi)
+        {
+            Emi_Discount(LoanAmount,relativeMarking,tenure,interest,out emi);
+            salary = salary - emi;
+            Console.WriteLine("Tu salario es: " + salary);
         }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary1;
 
 namespace ConsoleApp1
 {
@@ -10,6 +11,12 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int loanAmount, interest, emi;
+                float salary=26000;
+
+            LoanCalculation ViejaPuta = new LoanCalculation();
+            ViejaPuta.ApplyForLoan(salary,out loanAmount,out interest,"Pasaporte","curp");
+            ViejaPuta.SalaryDeduction(ref salary,2,5,interest,loanAmount,out emi);
         }
     }
 }
